eb4a0136b32259f8c1bcce9333ebc467f1bdd26c
quipildorpro <quipildorprogramacion@gmail.com>
2025-08-14 17:39:21 -0300

    add nosotros

---
+++ nosotros.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+  </head>
+  <body>
+<header>
+    <nav class="navbar navbar-expand-lg bg-dark " data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+        <img width="400px" src="img/logo-dual.jpg" alt="">
+    </a>
+   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav ms-auto">
+        <a class="nav-link " aria-current="page" href="index.php">Home</a>
+        <a class="nav-link" href="productos.php">Productos</a>
+        <a class="nav-link" href="nosotros.php">Nosotros</a>
+        <a class="nav-link" href="contacto.php">Contacto</a>
+        
+      </div>
+    </div>
+  </div>
+</nav>
+</header>     
+
+<main>
+       <h1 class="text-center">Bienvenidos a Dual Core</h1>
+  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+  <h2 class="text-center">Tu nuevo aliado en tecnología e insumos informáticos</h2><br>
+   <div>
+   <img width= "800px" class="d-block mx-auto"  src="img/tienda.jpg" alt="foto tienda">
+</div>
+<br>
+  <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <p>Fundada en marzo de 2025, "Dual Core" nace con una misión clara: ofrecer soluciones tecnológicas confiables, rápidas y al mejor precio para particulares, profesionales y empresas. Sabemos lo importante que es contar con equipos y accesorios de calidad, y por eso nos dedicamos exclusivamente a ofrecer los mejores insumos informáticos, con stock actualizado y atención personalizada.</p>
+            </div>
+             <a class="icon-link" href="#">
+  <svg xmlns="http://www.w3.org/2000/svg" class="bi" viewBox="0 0 16 16" aria-hidden="true">
+    <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2l-2.218-.887zm3.564 1.426L5.596 5 8 5.961 14.154 3.5l-2.404-.961zm3.25 1.7-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923l6.5 2.6zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464L7.443.184z"/>
+  </svg>
+   ¿Qué ofrecemos?</a>
+   <a>
+<p> Cartuchos y tóners para impresoras</p>
+<p> Teclados, mouse, auriculares y periféricos</p>
+<p> Memorias RAM, discos SSD, cables y adaptadores</p>
+<p> Equipos de red: routers, switches y más</p>
+<p> Accesorios para notebooks y PCs</p>
+<p> Productos para mantenimiento y limpieza</p>
+</a>
+   
+<a class="icon-link icon-link-hover" href="#">
+¿Por qué elegirnos?
+<svg xmlns="http://www.w3.org/2000/svg" class="bi" viewBox="0 0 16 16" aria-hidden="true">
+    <path d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+  </svg>
+</a>
+<a>
+<p> Productos 100% originales</p>
+<p> Precios competitivos</p>
+<p> Asesoramiento personalizado</p>
+<p> Entregas rápidas y seguras</p>
+<p> Atención posventa garantizada</p>
+</a>
+
+
+<hr>Ya sea que estés armando una PC, reponiendo insumos de oficina o buscando mejorar tu setup gamer, Dual Core está para ayudarte a elegir lo mejor, sin vueltas</hr>
+
+
+
+    </div>
+
+    </div>
+
+
+
+</main>
+
+
+<footer class="p-5 bg-dark">
+    <p class="text-center text-white">todos los derechos reservados 2025- DUAL CORE</p>
+</footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+  </body>
+</html>
