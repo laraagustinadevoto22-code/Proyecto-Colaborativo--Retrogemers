029217c4e7b4a4dc71520f15179b41c09e97ef8a
Char2025arg <carogarive@gmail.com>
2025-08-14 17:50:32 -0300

    Caro add productos

---
+++ productos.php
@@ -0,0 +1,133 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+  </head>
+  <body>
+<header>
+    <nav class="navbar navbar-expand-lg bg-dark " data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+        <img width="400px" src="img/logo-dual.jpg" alt="">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav ms-auto">
+        <a class="nav-link " aria-current="page" href="index.php">Home</a>
+        <a class="nav-link" href="productos.php">Productos</a>
+        <a class="nav-link" href="nosotros.php">Nosotros</a>
+        <a class="nav-link" href="contacto.php">Contacto</a>
+        
+      </div>
+    </div>
+  </div>
+</nav>
+</header>     
+
+<main class="container">
+
+ <h1 class="text-center">Productos</h1>
+
+<div class="row">
+ 
+    <div class="col-4">
+        <div class="card mb-4" style="width: 22rem;">
+            <img height="400px"   src="img/memoriaram.png" class="card-img-top" alt="">
+            <div class="card-body">
+                <p class="fs-6 fw-bold text-danger"></p>
+                <h5 class="card-title"></h5>
+                <p class="card-text"></p>
+            </div>
+         
+            <p class="text-primary"> <b> Memoria RAM Fury Beast DDR4 gamer color negro 8GB 1 Kingston KF432C16BB/8</b></p>
+        </div>
+    </div>
+
+        <div class="col-4">
+        <div class="card mb-3" style="width:22rem;">
+            <img height="400px"  src="img/gabinete.png" class="card-img-top" alt="">
+            <div class="card-body">
+                <p class="fs-6 fw-bold text-danger"></p>
+                <h5 class="card-title"></h5>
+                <p class="card-text"></p>
+            </div>
+         <p class="text-primary"><b>Gabnete Xyz Neutron White 4fans A-rgb 120mm Controlador Dual Chamber</b> </p>
+
+            
+        </div>
+    </div>
+
+        <div class="col-4">
+        <div class="card mb-3" style="width:22rem;">
+            <img height="400px"  src="img/fuentepc.png" class="card-img-top" alt="">
+            <div class="card-body">
+                <p class="fs-6 fw-bold text-danger"></p>
+                <h5 class="card-title"></h5>
+                <p class="card-text"></p>
+            </div>
+         <p class="text-primary"><b>Fuente Gigabyte Ud1000gm 1000w 80 Plus Gold</b> </p>
+            
+        </div>
+    </div>
+   
+
+    <div class="col-4">
+        <div class="card mb-3" style="width:22rem;">
+            <img height="400px"  src="img/motherboards.png" class="card-img-top" alt="">
+            <div class="card-body">
+                <p class="fs-6 fw-bold text-danger"></p>
+                <h5 class="card-title"></h5>
+                <p class="card-text"></p>
+            </div>
+         <p class="text-primary"><b> Motherboard Asus Tuf Gaming B550m-plus Wifi 2</b> </p>
+            
+        </div>
+    </div>
+
+<div class="col-4">
+        <div class="card mb-3" style="width:22rem;">
+            <img height="400px"  src="img/placa de sonido.png" class="card-img-top" alt="">
+            <div class="card-body">
+                <p class="fs-6 fw-bold text-danger"></p>
+                <h5 class="card-title"></h5>
+                <p class="card-text"></p>
+            </div>
+         <p class="text-primary"><b> Interfaz Behringer U-phoria Umc204 Hd 24 Bit/192khz - Om Color Negro </b> </p>
+            
+        </div>
+    </div>
+
+
+    <div class="col-4">
+        <div class="card mb-3" style="width:22rem;">
+            <img height="400px"  src="img/procesador.png" class="card-img-top" alt="">
+            <div class="card-body">
+                <p class="fs-6 fw-bold text-danger"></p>
+                <h5 class="card-title"></h5>
+                <p class="card-text"></p>
+            </div>
+         <p class="text-primary"><b> Procesador AMD Ryzen 7 5700G 100-100000263BOX de 8 núcleos y 4.6GHz de frecuencia con gráfica integrada</b> </p>
+            
+        </div>
+    </div>
+
+
+</div>
+
+
+
+
+</main>
+
+
+<footer class="p-5 bg-dark">
+    <p class="text-center text-white">todos los derechos reservados 2025- DUAL CORE</p>
+</footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+  </body>
+</html>
